e42c30ed99286fd08e0829724c2981fb591adb62
Kaho.A <papagei0303@gmail.com>
2024-10-17 17:53:24 +0200

    fix: Fixed CORS header
    Based on teacher's suggestion to just keep the *

--- backend/database/server.js
+++ backend/database/server.js
@@ -15,7 +15,7 @@ const port = 3000;
  // Socket.io-Server
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:*", "http://127.0.0.1:*"],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
