a9d1f8ea5fd8e57e13a9eea3604a6580d8456ec6
Marcus <biebermarcus1@gmail.com>
2024-10-17 17:00:38 +0200

    fix: Herstellen einer einzelnen stabilen Verbindung

--- frontend/src/Components/TextInput.jsx
+++ frontend/src/Components/TextInput.jsx
@@ -1,21 +1,28 @@
 import io from "socket.io-client";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function TextInput() {
-  const socket = io("http://localhost:3000");
+  const [message, setMessage] = useState("");
+  const socketRef = useRef(null);
 
-  socket.on("connect_error", (err) => {
-    console.error(`Verbindungsfehler: ${err.message}`);
-  });
+  useEffect(() => {
+    socketRef.current = io("http://localhost:3000");
+
+    socketRef.current.on("connection_error", (err) => {
+      console.error(`Verbindungsfehler: ${err.message}`);
+    });
+
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
 
   const sendMessage = () => {
-    socket.emit("send_message", { message: "Hallo aus dem Frontend" });
+    if (socketRef.current) {
+      socketRef.current.emit("send_message", { message });
+      setMessage("");
+    }
   };
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      alert(data.message);
-    });
-  }, [socket]);
 
   return (
     <div style={{ position: "relative", width: "100%" }}>
@@ -23,6 +30,8 @@ function TextInput() {
         type="text"
         id="input"
         placeholder="schreib' eine Nachricht..."
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
         style={{
           borderRadius: "20px",
           padding: "40px 50px 40px 20px",
